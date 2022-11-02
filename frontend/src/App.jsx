f8dc1a3f342e7b13fc2eda9c6f51773bade138ac
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2022-11-02 23:51:42 +1100

    WiP FS1, @mui time

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import { Context, initialValue } from './context';
-import { signUp, logIn } from './components/Auth';
-import { Navbar } from './components/Navbar';
+// import { signUp, logIn } from './components/Auth';
+// import { Navbar } from './components/Navbar';
 
 const App = () => {
-  
   const [token, setToken] = React.useState(initialValue.token);
   const [loggedInState, setLoggedInState] = React.useState(initialValue.loggedIn);
+
   const getters = {
     token,
     loggedInState,
@@ -25,13 +25,11 @@ const App = () => {
     setToken,
     setLoggedInState,
   };
-  
-  //**************************************************************************/
-  //**                      Feature Set 1 - Admin Auth                      **/
-  //**************************************************************************/
 
-  
-  
+  //* ********************************************************************** */
+  //*                       Feature Set 1 - Admin Auth                       */
+  //* ********************************************************************** */
+
   return (
     <Context.Provider value={{ getters, setters, }}>
       <body>
--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,9 +1,206 @@
 import React from 'react';
 
-export const SignUp = () => {
+import { Context, useContext } from './../context';
+import config from './../config.json';
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+//* ********************************************************************** */
+//*                       Feature Set 1 - Admin Auth                       */
+//* ********************************************************************** */
+
+export const SignUp = () => {
 }
 
 export const LogIn = () => {
+    const { getters, setters } = useContext(Context);
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async (email, password) => {
+        const url = config.PREPORT_URL + config.BACKEND_PORT + "/user/auth/login";
+
+        let body_data = {
+            email: email,
+            password: password
+        }
+        try {
+            const res = await fetch(url, { 
+                method: "POST",
+                headers: { 
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body_data)
+            });
+            const data = await res.json();
+            setters.setToken(data.token);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="Hello World"
+            />
+            <TextField
+              disabled
+              id="outlined-disabled"
+              label="Disabled"
+              defaultValue="Hello World"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <TextField
+              id="outlined-read-only-input"
+              label="Read Only"
+              defaultValue="Hello World"
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+            <TextField
+              id="outlined-number"
+              label="Number"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField id="outlined-search" label="Search field" type="search" />
+            <TextField
+              id="outlined-helperText"
+              label="Helper text"
+              defaultValue="Default Value"
+              helperText="Some important text"
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="filled-required"
+              label="Required"
+              defaultValue="Hello World"
+              variant="filled"
+            />
+            <TextField
+              disabled
+              id="filled-disabled"
+              label="Disabled"
+              defaultValue="Hello World"
+              variant="filled"
+            />
+            <TextField
+              id="filled-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="filled"
+            />
+            <TextField
+              id="filled-read-only-input"
+              label="Read Only"
+              defaultValue="Hello World"
+              InputProps={{
+                readOnly: true,
+              }}
+              variant="filled"
+            />
+            <TextField
+              id="filled-number"
+              label="Number"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="filled"
+            />
+            <TextField
+              id="filled-search"
+              label="Search field"
+              type="search"
+              variant="filled"
+            />
+            <TextField
+              id="filled-helperText"
+              label="Helper text"
+              defaultValue="Default Value"
+              helperText="Some important text"
+              variant="filled"
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="standard-required"
+              label="Required"
+              defaultValue="Hello World"
+              variant="standard"
+            />
+            <TextField
+              disabled
+              id="standard-disabled"
+              label="Disabled"
+              defaultValue="Hello World"
+              variant="standard"
+            />
+            <TextField
+              id="standard-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="standard"
+            />
+            <TextField
+              id="standard-read-only-input"
+              label="Read Only"
+              defaultValue="Hello World"
+              InputProps={{
+                readOnly: true,
+              }}
+              variant="standard"
+            />
+            <TextField
+              id="standard-number"
+              label="Number"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="standard"
+            />
+            <TextField
+              id="standard-search"
+              label="Search field"
+              type="search"
+              variant="standard"
+            />
+            <TextField
+              id="standard-helperText"
+              label="Helper text"
+              defaultValue="Default Value"
+              helperText="Some important text"
+              variant="standard"
+            />
+          </div>
+        </Box>
+      );
 }
