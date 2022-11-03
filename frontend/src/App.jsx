c3e2dde0fe48b40fcd9e860858eb5af5befabba3
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2022-11-03 23:36:24 +1100

    WiP FS1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 // import logo from './logo.svg';
 import './App.css';
 
 import { Context, initialValue } from './context';
-// import { signUp, logIn } from './components/Auth';
+import { Register, LogIn } from './components/Auth';
 // import { Navbar } from './components/Navbar';
+
+import { ReactComponent as AirbnbLogo } from './images/Airbnb_Logo_Belo.svg';
+
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const App = () => {
   const [token, setToken] = React.useState(initialValue.token);
@@ -30,10 +41,67 @@ const App = () => {
   //*                       Feature Set 1 - Admin Auth                       */
   //* ********************************************************************** */
 
+  // const { getters } = useContext(Context);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Context.Provider value={{ getters, setters, }}>
-      <body>
-      </body>
+      <Router>
+        <body>
+          <nav>
+            <Link to='/'>
+              <AirbnbLogo
+                src="./images/Airbnb_Logo_Belo.svg"
+                alt="Airbnb Logo"
+              />
+            </Link>
+            <Button
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <MenuIcon color='action'/>
+              <AccountCircleIcon color='action'/>
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <Link to='/register'>
+                <MenuItem onClick={handleClose}>Sign Up</MenuItem>
+              </Link>
+              <Link to='/login'>
+                <MenuItem onClick={handleClose}>Log In</MenuItem>
+              </Link>
+            </Menu>
+          </nav>
+          <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/">
+          </Route>
+        </Switch>
+        </body>
+      </Router>
     </Context.Provider>
   );
 }
--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -10,38 +10,130 @@ import TextField from '@mui/material/TextField';
 //*                       Feature Set 1 - Admin Auth                       */
 //* ********************************************************************** */
 
-export const SignUp = () => {
+export const Register = () => {
+  const { getters, setters } = useContext(Context);
+
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [passwordConf, setPasswordConf] = React.useState('');
+
+  const signup = async (name, email, password, passwordConf) => {
+    const url = config.PREPORT_URL + config.BACKEND_PORT + '/user/auth/register';
+
+    if (password !== passwordConf) {
+      throw new Error('Passwords don\'t match!!');
+    }
+
+    const bodyData = {
+      email: email,
+      password: password,
+      name: name
+    }
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bodyData)
+      });
+      const data = await res.json();
+      setters.setToken(data.token);
+      console.log(data.token);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <Context.Provider value={{ getters, setters, }}>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={(event) => {
+          event.preventDefault()
+          signup(name, email, password, passwordConf)
+        }}
+      >
+        <div>
+          <TextField
+            id='register-name-input'
+            label='Name'
+            value={name}
+            onInput={event => setName(event.target.value)}
+          />
+          <TextField
+            id='register-email-input'
+            label='Email'
+            value={email}
+            onInput={event => setEmail(event.target.value)}
+          />
+          <TextField
+            id='register-password-input'
+            label='Password'
+            type='password'
+            autoComplete='current-password'
+            value={password}
+            onInput={event => setPassword(event.target.value)}
+          />
+          <TextField
+            id='register-password-conf-input'
+            label='Password Confirmation'
+            type='password'
+            autoComplete='current-password'
+            value={password}
+            onInput={event => setPasswordConf(event.target.value)}
+          />
+          <button
+            id='register-button'
+            type="submit"
+          >
+            Register
+          </button>
+        </div>
+      </Box>
+    </Context.Provider>
+  );
 }
 
 export const LogIn = () => {
-    const { getters, setters } = useContext(Context);
+  const { getters, setters } = useContext(Context);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-    const login = async (email, password) => {
-        const url = config.PREPORT_URL + config.BACKEND_PORT + "/user/auth/login";
+  const login = async (email, password) => {
+    const url = config.PREPORT_URL + config.BACKEND_PORT + '/user/auth/login';
 
-        let body_data = {
-            email: email,
-            password: password
-        }
-        try {
-            const res = await fetch(url, { 
-                method: "POST",
-                headers: { 
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(body_data)
-            });
-            const data = await res.json();
-            setters.setToken(data.token);
-        } catch (err) {
-            console.log(err);
-        }
+    const bodyData = {
+      email: email,
+      password: password
     }
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bodyData)
+      });
+      const data = await res.json();
+      setters.setToken(data.token);
+      setters.setLoggedInState(true);
+      console.log(data.token);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-    return (
+  return (
+    <Context.Provider value={{ getters, setters, }}>
+      <body>
         <Box
           component="form"
           sx={{
@@ -49,23 +141,35 @@ export const LogIn = () => {
           }}
           noValidate
           autoComplete="off"
+          onSubmit={(event) => {
+            event.preventDefault()
+            login(email, password)
+          }}
         >
           <div>
             <TextField
-              required
-              id="outlined-required"
-              label="Email"
-              defaultValue=""
+              id='login-email-input'
+              label='Email'
+              value={email}
               onInput={event => setEmail(event.target.value)}
             />
             <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
+              id='login-password-input'
+              label='Password'
+              type='password'
+              autoComplete='current-password'
+              value={password}
               onInput={event => setPassword(event.target.value)}
             />
+            <button
+              id='log-in-button'
+              type="submit"
+            >
+              Log In
+            </button>
           </div>
         </Box>
-      );
+      </body>
+    </Context.Provider>
+  );
 }
